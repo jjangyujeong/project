1adc26f5bbe4fcbc51e8e06e7f63cd625f31c6e9
Woojae Kim <ssaskick@gmail.com>
2015-11-19 18:39:31 +0900

    modified sound_d/main.c/

--- sound_d/main.c
+++ sound_d/main.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#define AAA 100
+//#define AAA 100
 //#define BBB 200
 
 int main(int argc, char argv[])
 {
-	printf("please Enter wav files\n");
-	printf("playing wav file\n");
-	//system("aplay test.wav");
+	//printf("please Enter wav files\n");
+	printf("Converting mp3 files to wav files\n");
+	system("mpg123 -w allday.wav allday.mp3");
+	printf("Playing wav files\n");
+	//printf("HAHAHA\n");
 	system("aplay allday.wav");
-	//conver mp3 files to wav files
-	system("mpg123 -w example.wav allday.mp3");
-	printf("HAHAHA\n");
 	return 0;
 }
