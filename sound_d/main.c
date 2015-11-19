b62523232db6b6bccd715e03f982138aac1985a8
Woojae Kim <ssaskick@gmail.com>
2015-11-19 19:02:33 +0900

    added test.wav file

--- sound_d/main.c
+++ sound_d/main.c
@@ -4,11 +4,14 @@
 
 int main(int argc, char argv[])
 {
+
+
 	//printf("please Enter wav files\n");
 	printf("Converting mp3 files to wav files\n");
 	system("mpg123 -w allday.wav allday.mp3");
 	printf("Playing wav files\n");
 	//printf("HAHAHA\n");
-	system("aplay allday.wav");
+	//system("aplay allday.wav");
+	system("aplay test.wav");
 	return 0;
 }
