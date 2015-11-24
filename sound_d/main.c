121f43c936197490979f29b3f554d7ff887f0265
Woojae Kim <ssaskick@gmail.com>
2015-11-24 22:51:51 +0900

    added include dir

--- sound_d/main.c
+++ sound_d/main.c
@@ -2,16 +2,19 @@
 //#define AAA 100
 //#define BBB 200
 
+void playsound()
+{
+	//printf("please Enter wav files\n");
+        printf("Converting mp3 files to wav files\n");
+        system("mpg123 -w allday.wav allday.mp3");
+        printf("Playing wav files\n");
+        //printf("HAHAHA\n");
+        system("aplay allday.wav");
+        //system("aplay test.wav");
+
+}
 int main(int argc, char argv[])
 {
-
-
-	//printf("please Enter wav files\n");
-	printf("Converting mp3 files to wav files\n");
-	system("mpg123 -w allday.wav allday.mp3");
-	printf("Playing wav files\n");
-	//printf("HAHAHA\n");
-	//system("aplay allday.wav");
-	system("aplay test.wav");
+	playsound();
 	return 0;
 }
